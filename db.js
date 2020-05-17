bba098019308de3380916d83cbe0c8339627770e
Ubuntu <ubuntu@ip-172-31-95-177.ec2.internal>
2020-05-17 08:00:21 +0000

    Removed table creation for new users

--- db.js
+++ db.js
@@ -38,14 +38,7 @@ function userSignup(userData, callback, exists, error) {
                     console.error('error query: ' + err.stack);
                     return error();
                 }
-                let sqlQuery = "CREATE TABLE userTrips_" + results[1][0].user_id + " (id INT(24) NOT NULL AUTO_INCREMENT, trip_id INT(24) NOT NULL, PRIMARY KEY (`id`))";
-                pool.query(sqlQuery, function (err, result, fields) {
-                    if (err) {
-                        console.error('error query: ' + err.stack);
-                        return error();
-                    }
-                    return callback(results[1]);
-                });
+                return callback(results[1]);
             });
         } else {
             return exists();
